7a51ea1b01ddf2b785104503f5372fdcf6674c34
prospero78su <prospero.78.su@gmail.com>
2019-05-17 17:15:30 +0300

    06 Добавление функциональности для отображения сайта

--- main.go
+++ main.go
@@ -3,7 +3,14 @@ package main
 /*
 	Главный модуль для запуска персонального сайта prospero78su
 */
+import (
+	мСвеб "github.com/prospero78/mySite/пакСерверВеб"
+	мВрем "time"
+)
 
 func main() {
-
+	go мСвеб.ВебСервер.Старт()
+	for {
+		мВрем.Sleep(мВрем.Second * 10)
+	}
 }
