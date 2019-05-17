ec276d883a11a3ad628775e14d851c3b475867ab
prospero78su <prospero.78.su@gmail.com>
2019-05-17 17:15:00 +0300

    05 Добавление кубиков для страниц

---
+++ пакСерверВеб/пакСтрВеб/модСтрМеню.go
@@ -0,0 +1,16 @@
+package пакСтрВеб
+
+/*
+	Модуль предоставляет меню для страниц
+*/
+
+const (
+	меню = `
+<LEFT>
+<a href="/">[Главная]</a>&nbsp;&nbsp;
+<a href="/">[Об авторе]</a>&nbsp;&nbsp;
+</LEFT>
+<BR>
+`
+	шрифтМеню = `<font size="5" color="blue" face="Arial">`
+)
